1e2e2438b2016116d65f55844c138d85acb4496b
Humble <humberto221099@gmail.com>
2020-04-21 19:27:54 -0500

    Ajuste de las credenciales

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://heroku:du1llIP49P93YzPp@cluster0-ycito.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
